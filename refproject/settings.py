e2d50afc0157db8e88b5bd4139c3eb70b3697258
chaitanya <44922027+chaitphani@users.noreply.github.com>
2020-06-06 11:44:00 +0530

    changing database in project

--- refproject/settings.py
+++ refproject/settings.py
@@ -76,17 +76,9 @@ WSGI_APPLICATION = 'refproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hrmdb',
-        'USER': 'hrmuser',
-        'PASSWORD': 'hrmpwd#',
-        'HOST': '192.168.5.10',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
